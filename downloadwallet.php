e764df9e3910d98f0963b776431bfa48eb21f5db
Craig <craig.b.macgregor@gmail.com>
2017-07-19 14:46:39 +1200

    changed dir to backup to

--- downloadwallet.php
+++ downloadwallet.php
@@ -2,9 +2,9 @@
 include ("header.php");
 include ("pass.php");
 $filename = time() . "_wallet.dat";
-$destination = "/home/stakebox/UI/" . $filename;
+$destination = "/home/stakebox/UI/backups" . $filename;
 $coin->backupwallet($destination);
 exec("sudo chmod 775 " . $destination);
-header("Location: http://" . $_SERVER['SERVER_NAME']."/" . $filename);
+header("Location: http://" . $_SERVER['SERVER_NAME']."/backups/" . $filename);
 exit;
 ?>
