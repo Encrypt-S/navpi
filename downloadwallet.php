6fa962254fbb654c5009e3bb327c252ea608b4b1
Craig <craig.b.macgregor@gmail.com>
2017-07-19 14:48:11 +1200

    added slash

--- downloadwallet.php
+++ downloadwallet.php
@@ -2,7 +2,7 @@
 include ("header.php");
 include ("pass.php");
 $filename = time() . "_wallet.dat";
-$destination = "/home/stakebox/UI/backups" . $filename;
+$destination = "/home/stakebox/UI/backups/" . $filename;
 $coin->backupwallet($destination);
 exec("sudo chmod 775 " . $destination);
 header("Location: http://" . $_SERVER['SERVER_NAME']."/backups/" . $filename);
