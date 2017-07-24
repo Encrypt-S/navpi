a1efe2e921ca6f54eccf99bca8e179f4596f2728
Craig <craig.b.macgregor@gmail.com>
2017-07-24 12:11:53 +1200

    updated instructions

--- downloadwallet.php
+++ downloadwallet.php
@@ -1,14 +1,17 @@
 <?php
 include ("header.php");
 include ("pass.php");
-$filename = "wallet.dat";
+
+$filename = time() . "_wallet.dat";
 $destination = "/home/pi/Desktop/backup/" . $filename;
 ?>
 
 <?php
 try {
 	$coin->backupwallet($destination);
-	echo "<p class='bg-success'><b>You have successfully backed your wallet up to the desktop. Please login to your Nav Pi to retrieve the backup.</b></p> ";
+	echo "<p class='bg-success'><b>You have successfully backed up your wallet.</b></p> ";
+  echo "<p><b>Please login to your Nav Pi to retrieve $filename from the back up folder on the desktop (/home/pi/Desktop/backup).</b></p>"
+  echo "<p><b>When restoring your wallet, rename the file to wallet.dat</b></p>"
 } catch(Exception $e) {
 	echo "<p class='bg-danger'><b>Error: Something went wrong...  Did you enter a location that does not exist, or is unreachable?</b></p>";
 }
