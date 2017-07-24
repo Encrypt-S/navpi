8043b5b9f26b9ab122b04fd230686d83044a98c4
Craig <craig.b.macgregor@gmail.com>
2017-07-24 12:12:43 +1200

    adding semi colons

--- downloadwallet.php
+++ downloadwallet.php
@@ -10,8 +10,8 @@ $destination = "/home/pi/Desktop/backup/" . $filename;
 try {
 	$coin->backupwallet($destination);
 	echo "<p class='bg-success'><b>You have successfully backed up your wallet.</b></p> ";
-  echo "<p><b>Please login to your Nav Pi to retrieve $filename from the back up folder on the desktop (/home/pi/Desktop/backup).</b></p>"
-  echo "<p><b>When restoring your wallet, rename the file to wallet.dat</b></p>"
+  echo "<p><b>Please login to your Nav Pi to retrieve $filename from the back up folder on the desktop (/home/pi/Desktop/backup).</b></p>";
+  echo "<p><b>When restoring your wallet, rename the file to wallet.dat</b></p>";
 } catch(Exception $e) {
 	echo "<p class='bg-danger'><b>Error: Something went wrong...  Did you enter a location that does not exist, or is unreachable?</b></p>";
 }
