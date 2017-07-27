8f6314d49994042cceda55b28def2dcb8fa3ab14
Craig <craig.b.macgregor@gmail.com>
2017-07-28 11:01:19 +1200

    wallet download updated

--- downloadwallet.php
+++ downloadwallet.php
@@ -3,7 +3,6 @@ include ("header.php");
 include ("pass.php");
 
 $filename = time() . "_wallet.dat";
-$destination = "/home/pi/Desktop/backup/" . $filename;
 $destination2 = "/home/stakebox/UI/backup/" . $filename;
 ?>
 
@@ -11,11 +10,9 @@ $destination2 = "/home/stakebox/UI/backup/" . $filename;
 try {
 	$coin->backupwallet($destination2);
 
-	header("Location: http://" . $_SERVER['SERVER_NAME']."/backup/" . $filename); 
+	header("Location: http://" . $_SERVER['SERVER_NAME']."/backup/" . $filename);
 
 	echo "<p class='bg-success'><b>You have successfully backed up your wallet.</b></p> ";
-  echo "<p><b>Please login to your Nav Pi to retrieve $filename from the back up folder on the desktop (/home/pi/Desktop/backup).</b></p>";
-  echo "<p><b>When restoring your wallet, rename the file to wallet.dat</b></p>";
 } catch(Exception $e) {
 	echo "<p class='bg-danger'><b>Error: Something went wrong...  Did you enter a location that does not exist, or is unreachable?</b></p>";
 }
