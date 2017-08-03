4297296c856a6b1ea9e831107622520b066968b0
Craig <craig.b.macgregor@gmail.com>
2017-08-03 16:02:33 +1200

    debug removed

--- header.php
+++ header.php
@@ -147,8 +147,6 @@ if($coinGetInfo) {
 
 	if ($coinGetInfo['unlocked_until'] && (int) $coinGetInfo['unlocked_until'] > 0) {
 
-		echo "<br /><br /><br /><br /> <pre>" .$coinGetInfo['unlocked_until']. "</pre>";
-
 		$address = $coin->getaddressesbyaccount("")[0];
 
 		try {
