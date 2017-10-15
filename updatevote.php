038cc3addb19a079e14a6fb0fb04616aec4e7ab1
Craig <craig.b.macgregor@gmail.com>
2017-10-16 11:31:20 +1300

    reading config file

--- updatevote.php
+++ updatevote.php
@@ -10,8 +10,25 @@ include ("pass.php");
     // votefunding=1
     // votefunding=0
 
-    print_r("Updated Vote to " . ucfirst($_POST['vote']) . "<br><br>");
-    print_r("Please restart to apply the new settings <a href='/applyupdate.php?updated=true' class='btn btn-default' style='margin:0;'>Restart</a>");
+    $configPath = "/home/stakebox/.navcoin4/navcoin.conf";
+
+    if (!file_exists($configPath)) {
+      print_r("Something went wrong, navcoin.conf does not exist<br><br>");
+    } else {
+      $configFile = file_get_contents($configPath);
+
+      $current = file_get_contents($file_path);
+
+      print_r($current);
+
+      // $fileHandle = fopen($configPath, 'w');
+      // fwrite($fileHandle, $data_to_write);
+      // fclose($fileHandle);
+
+      print_r("Updated Vote to " . ucfirst($_POST['vote']) . "<br><br>");
+      print_r("Please restart to apply the new settings <a href='/applyupdate.php?updated=true' class='btn btn-default' style='margin:0;'>Restart</a>");
+
+    }
   } else {
     print_r("Something went wrong, please try again<br><br>");
   }
