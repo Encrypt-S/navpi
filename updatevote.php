24b28f8b0690cac93ee12f0ac5aeeb54f18ed2aa
Craig <craig.b.macgregor@gmail.com>
2017-10-16 09:56:35 +1300

    corrected typo

--- updatevote.php
+++ updatevote.php
@@ -10,7 +10,7 @@ include ("pass.php");
     // votefunding=1
     // votefunding=0
 
-    print_r("Updated Vote to " . ucfirst($_POST['server']) . "<br><br>");
+    print_r("Updated Vote to " . ucfirst($_POST['vote']) . "<br><br>");
     print_r("Please restart to apply the new settings <a href='/applyupdate.php?updated=true' class='btn btn-default' style='margin:0;'>Restart</a>");
   } else {
     print_r("Something went wrong, please try again<br><br>");
