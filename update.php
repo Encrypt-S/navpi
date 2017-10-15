fe7d445866cd8b1f2ea843d48ed83da99272a731
Craig <craig.b.macgregor@gmail.com>
2017-10-16 12:05:37 +1300

    added info about updating daemon manually

--- update.php
+++ update.php
@@ -6,7 +6,8 @@ $walletDir = "/home/stakebox/wallets/".$currentWallet;
 
 <div style="padding: 5px 30px 0px;">
     <div class="row">
-        <div class="col-lg-2">
+        <div class="col-lg-12">
+            <b>Nav Pi User Interface</b><br />
             <form action="update" method="POST"><input type="hidden">
                 <div class="input-group">
      	            <button class='btn btn-default' type="submit" name="status" value="webui">Update WebUI</button>
@@ -14,6 +15,15 @@ $walletDir = "/home/stakebox/wallets/".$currentWallet;
             </form>
         </div><!-- /.col-lg-2 -->
     </div><!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+          <b>Nav Coin Core</b><br />
+           This device should automatically check for new versions of the Nav Coin Core wallet and update from github. If you want to run the updater manually, please run the following in the navpi terminal window.
+           <pre>
+             sudo /home/stakebox/UI/libs/updater.sh
+           </pre>
+        </div>
+    </div>
 <?php
 $status = $_POST["status"];
 
