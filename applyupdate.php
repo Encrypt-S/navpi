7fd2003d95822651e9e2ead4cdc97efeeb55edee
Craig <craig.b.macgregor@gmail.com>
2017-10-15 16:30:05 +1300

    trying different method to clear file

--- applyupdate.php
+++ applyupdate.php
@@ -2,30 +2,12 @@
 include ("header.php");
 include ("pass.php");
 $walletDir = "/home/stakebox/wallets/".$currentWallet;
-?>
 
-<div style="padding: 5px 30px 0px;">
-    <div class="row">
-        <div class="col-lg-2">
-            <form action="update" method="POST"><input type="hidden">
-                <div class="input-group">
-     	            <button class='btn btn-default' type="submit" name="status" value="webui">Update WebUI</button>
-                </div><!-- /input-group -->
-            </form>
-        </div><!-- /.col-lg-2 -->
-    </div><!-- /.row -->
-<?php
 $updated = $_GET["updated"];
 
-if ($updated == "true"){
-    exec("echo /dev/null > /home/stakebox/UI/update.json");
-    print '<h2>Restarting your device to complete update.</h2>';
-    exec("sudo shutdown -r now");
-} else {
-    exec("echo /dev/nul > /home/stakebox/UI/update.json");
-    print '<h2>Restarting your device to clear update error.</h2>';
-    exec("sudo shutdown -r now");
-}
+exec("truncate -s 0 /home/stakebox/UI/update.json");
+print '<h2>Clearing update message and restarting device.</h2>';
+exec("sudo shutdown -r now");
 
 ?>
 </div>
