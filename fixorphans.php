0581b27216f00f889682a1a20e3d22355ed04353
Craig <craig.b.macgregor@gmail.com>
2017-11-25 14:43:00 +1300

    trying again

--- fixorphans.php
+++ fixorphans.php
@@ -5,8 +5,9 @@ $walletDir = "/home/stakebox/wallets/".$currentWallet;
 
 $updated = $_GET["updated"];
 
+exec("killall navcoind");
 print '<h2>Restarting the navcoin daemon and flushing out txes. This can take up to 15 minutes.</h2>';
-exec("killall navcoind && /usr/local/bin/navcoind -datadir=/home/stakebox/.navcoin4 -zapwallettxes");
+exec("/usr/local/bin/navcoind -datadir=/home/stakebox/.navcoin4 -zapwallettxes");
 
 ?>
 </div>
