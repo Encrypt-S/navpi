7197d628db28d63f406606d9181e57b610a28640
Craig <craig.b.macgregor@gmail.com>
2017-11-25 15:05:32 +1300

    corrected background restart.

--- fixorphans.php
+++ fixorphans.php
@@ -7,7 +7,7 @@ $status = $_POST["status"];
 
 if ($status == "zapwallettxes") {
   exec("killall navcoind");
-  exec("/usr/local/bin/navcoind -datadir=/home/stakebox/.navcoin4 -zapwallettxes &");
+  exec("/usr/local/bin/navcoind -datadir=/home/stakebox/.navcoin4 -zapwallettxes > /dev/null &");
   print("<h2>Orphans are being fixed, navcoind is restarting</h2>");
 } else { ?>
   <h2>Fix Orphan Stakes</h2>
