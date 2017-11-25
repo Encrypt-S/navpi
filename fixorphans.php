bae9331172961121ee33b505af222e1eae0bbe16
Craig <craig.b.macgregor@gmail.com>
2017-11-25 14:41:35 +1300

    fixed teh restart path

--- fixorphans.php
+++ fixorphans.php
@@ -5,9 +5,8 @@ $walletDir = "/home/stakebox/wallets/".$currentWallet;
 
 $updated = $_GET["updated"];
 
-exec("killall navcoind");
 print '<h2>Restarting the navcoin daemon and flushing out txes. This can take up to 15 minutes.</h2>';
-exec("./usr/local/bin/navcoind -datadir=/home/stakebox/.navcoin4 -zapwallettxes");
+exec("killall navcoind && /usr/local/bin/navcoind -datadir=/home/stakebox/.navcoin4 -zapwallettxes");
 
 ?>
 </div>
