b7cdbae7a6a2b8962797c265dfc96b1d382631fb
Craig <craig.b.macgregor@gmail.com>
2017-12-13 18:17:38 +0100

    view update for stakervote

--- index.php
+++ index.php
@@ -93,9 +93,9 @@ if ($currentWallet == NavCoin){
 			$configPath = "/home/stakebox/.navcoin4/navcoin.conf";
 			$configFile = file_get_contents($configPath);
 
-			if (strpos($configFile, "votefunding=1")) {
+			if (strpos($configFile, "stakervote=1")) {
 				$vote = "YES";
-			} else if (strpos($configFile, "votefunding=0")) {
+			} else if (strpos($configFile, "stakervote=0")) {
 				$vote = "NO";
 			} else {
 				$vote = "NOT_VOTED";
