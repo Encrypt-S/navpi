900d5b2b4983a150d2b8577f1f1bd3bc1ab798e9
Matt <mpau548@aucklanduni.ac.nz>
2018-09-27 16:21:45 +1200

    swapped print command

--- cf-commands.php
+++ cf-commands.php
@@ -9,20 +9,20 @@ switch ($command) {
   case "createproposal":
     list($nav, $address, $time, $description) = explode(' ',$input[1], 4);
     $rpc_command = array($command, $nav, $address, $time, $description);
-    echo "<p>command: $rpc_command</p>";
+    print_r( "<p>command: $rpc_command</p>");
     printarray($coin->$rpc_command());
     break;
   case "createpaymentrequest":
     list($hash, $nav, $unique_id) = explode(' ',$input[1], 3);
     $rpc_command = array($command, $hash, $nav, $unique_id);
-    echo "<p>command: $rpc_command</p>";
+    print_r( "<p>command: $rpc_command</p>");
     printarray($coin->$rpc_command());
     break;
   case "proposalvote":
   case "paymentrequestvote":
     list($hash, $option) = explode(' ', $input[1]);
     $rpc_command = array($command, $hash, $option);
-    echo "<p>command: $rpc_command</p>";
+    print_r( "<p>command: $rpc_command</p>");
     printarray($coin->$rpc_command());
     break;
 }
