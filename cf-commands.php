8a625dfed7f421373ab940e1aa2b968ecef76c14
Matt <mpau548@aucklanduni.ac.nz>
2018-09-27 15:51:46 +1200

    added basic CF logic

---
+++ cf-commands.php
@@ -0,0 +1,27 @@
+<?php
+include ("header.php");
+include ("pass.php");
+$input = explode(' ', $_POST['order'], 2);
+
+$command = $input[0];
+
+switch ($command) {
+  case "createproposal":
+    list($nav, $address, $time, $description) = explode($input[1], 4);
+    echo("<p>{$nav}, {$address}, {$time}, {$description}</p>");
+    break;
+  case "createpaymentrequest":
+    list($hash, $nav, $unique_id) = explode($input[1], 3);
+    echo("<p>{$hash}, {$nav}, {$unique_id}</p>");
+    break;
+  case "proposalvote":
+  case "paymentrequestvote":
+    list($hash, $option) = explode($input[1]);
+    echo("<p>{$hash}, {$option}</p>");
+    break;
+}
+
+
+?>
+
+<?php include ("footer.php");?>
