d5b39c11069a214160bff95a98b93ffd8cb8214a
Matt <mpau548@aucklanduni.ac.nz>
2018-10-08 11:29:36 +1300

    added string cropping

--- cf-commands.php
+++ cf-commands.php
@@ -5,18 +5,25 @@ $input = explode(' ', $_POST['order'], 2);
 
 $command = $input[0];
 
-
 switch ($command) {
   case "createproposal":
   try {
-    
-
       list($address, $nav, $time, $description) = explode(' ',$input[1], 4);
       $nav = (int)$nav;
       $time = (int)$time;
-      echo("<p><b> $command $nav $address $time $description </b></p>");
-      // $rpc_command = array($command, $nav, $address, $time, $description);
-      print_r( "<p>command: $command, $nav, $address, $time, $description</p>");
+
+      $firstCharacter = substr($description, 0,1);
+      $lastCharacter = substr($description, -1);
+      if ($firstCharacter == '"') {
+        $description = substr($description, 1)
+      }
+
+      if ($lastCharacter == '"') {
+        $description = substr($description, 0, -1)
+      }
+
+      echo("<p><b>$firstCharacter $lastCharacter</b></p>");
+      echo("<p><b>$command $address $nav $time $description</b></p>");
       printarray($coin->$command($address, $nav, $time, $description));
     } catch(Exception $e) {
       echo "<p class='bg-danger'><b>{$e}Error: Something went wrong... Double check for spelling error and correct syntax. </b></p>";
@@ -25,8 +32,22 @@ switch ($command) {
   case "createpaymentrequest":
     try {
       list($hash, $nav, $unique_id) = explode(' ',$input[1], 3);
-      // $rpc_command = array($command, $hash, $nav, $unique_id);
-      print_r( "<p>command: $command, $hash, $nav, $unique_id</p>");
+      $nav = (int)$nav;
+
+
+      $firstCharacter = substr($unique_id, 0,1);
+      $lastCharacter = substr($unique_id, -1);
+      if ($firstCharacter == '"') {
+        $unique_id = substr($unique_id, 1)
+      }
+
+      if ($lastCharacter == '"') {
+        $unique_id = substr($unique_id, 0, -1)
+      }
+
+      echo("<p><b>$firstCharacter $lastCharacter</b></p>");
+
+      echo("<p><b>$command $hash $nav $unique_id</b></p>");
       printarray($coin->$command($hash, $nav, $unique_id));
     } catch(Exception $e) {
       echo "<p class='bg-danger'><b>{$e}Error: Something went wrong... Double check for spelling error and correct syntax. </b></p>";
@@ -36,7 +57,7 @@ switch ($command) {
   case "paymentrequestvote":
     try {
       list($hash, $option) = explode(' ', $input[1]);
-      // $rpc_command = array($command, $hash, $option);
+      echo("<p><b>$command $hash $option</b></p>");
       print_r( "<p>$command, $hash, $option</p>");
       printarray($coin->$command($hash, $option));
     } catch(Exception $e) {
